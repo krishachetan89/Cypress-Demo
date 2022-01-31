78df80f01b3154ffc7d3b69539e32221a98c7a00
krishna chetan <krchetan@in.ibm.com>
2022-01-31 16:13:00 +0530

    no message

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -7,20 +7,11 @@ describe("Verify the website", function() {
       this.sample = sample
     })
   })
-  it("verify the status of the site", function() {
-    cy.request("/about").should(function(response) {
-      expect(response.status).to.eq(200)
-    })
-  })
-  it("verify the heading of the blog", function() {
-    cy.get(".head-logo")
-      .children("a")
-      .should("have.text", this.sample.heading)
+  it("Click on accept cookies", function() {
+    cy.get('#coiPage-1 > .coi-banner__page-footer > .coi-button-group > .coi-banner__accept').click()
   })
   it("verfiy the image icon on the site", function() {
     console.log(this.sample, "printing data")
-    cy.get(".profile-img")
-      .should("have.attr", "src")
-      .and("include", this.sample.imageName)
+    cy.get('.content-heading')
   })
 })
