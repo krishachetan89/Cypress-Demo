299375dd9eacb6c0d6c111f5db35d3bfe7b75dcc
krishna chetan <krchetan@in.ibm.com>
2022-01-31 16:30:54 +0530

    added tests

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -8,10 +8,19 @@ describe("Verify the website", function() {
     })
   })
   it("Click on accept cookies", function() {
-    cy.get('#coiPage-1 > .coi-banner__page-footer > .coi-button-group > .coi-banner__accept').click()
+    cy.get('#coiPage-1 > .coi-banner__page-footer > .coi-button-group > .coi-banner__accept')
+      .click()
   })
-  it("verfiy the image icon on the site", function() {
+  it("Verify the image icon on the site", function() {
     console.log(this.sample, "printing data")
     cy.get('.content-heading')
+  })
+
+  it("Verify if user is able to click on search button", function() {
+    cy.get('.nav-item > .nav-link > .icon').click()
+  })
+
+  it("Verify if user is able to search for a value using search box", function() {
+    cy.get('#q').click().type(this.sample.Name)
   })
 })
